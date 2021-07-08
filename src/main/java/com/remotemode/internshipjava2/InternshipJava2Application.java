600ae3e849e254d1c5a4c5e073a80dfb1e1bea75
Iryna Holovata <irynahol@wizardsdev.com>
2021-07-08 14:02:29 +0300

    first commit

---
+++ src/main/java/com/remotemode/internshipjava2/InternshipJava2Application.java
@@ -0,0 +1,13 @@
+package com.remotemode.internshipjava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipJava2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipJava2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/remotemode/internshipjava2/InternshipJava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remotemode.internshipjava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipJava2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
